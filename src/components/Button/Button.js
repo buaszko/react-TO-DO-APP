b84c734e7a9b5844f2fe7ba7cd6ffa984355df75
buaszko <blazej.nagiecki@gmail.com>
2022-02-04 17:20:32 +0100

    added columns

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,6 @@ import styles from './Button.module.scss';
 
 const Button = props => {
     return <button className={styles.button}>{props.text} </button>
-
 }
 
 export default Button;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,6 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
+
 
 const List = () => {
     return (
@@ -7,17 +9,11 @@ const List = () => {
               <h2 className={styles.title}>Things to do<span>soon!</span></h2>
           </header>
           <p className={styles.description}>Interesting things I want to check out</p>
-          <section className={styles.columns}>
-              <article>
-                  <h2>Books</h2>
-              </article>
-              <article>
-                  <h2>Movies</h2>
-              </article>
-              <article>
-                  <h2>Games</h2>
-              </article>
-          </section>
+        <section className={styles.columns}>
+           <Column title="Books" />
+           <Column title="Movies" />
+           <Column title="Games" />
+        </section>
       </div>
     );
   };
